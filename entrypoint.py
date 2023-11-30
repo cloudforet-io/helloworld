a4b0805acafce000a84e2050775e98861c54d327
Choonho Son <choonhoson@megazone.com>
2023-11-30 15:28:52 +0900

    Add label
    Signed-off-by: Choonho Son <choonhoson@megazone.com>

--- entrypoint.py
+++ entrypoint.py
@@ -4,6 +4,7 @@ import json
 
 print("==== Commit URL ====")
 print(os.environ['INPUT_COMMITS_URL'])
+print(os.environ['INPUT_ISSUE_URL'])
 
 def find_signed_off_by(msg):
     """ find signed-off-by
@@ -11,6 +12,15 @@ def find_signed_off_by(msg):
     if msg.find("Signed-off-by:") == -1:
         return False
     return True
+
+def add_label(url, label="Signed-off-by"):
+    """ add label to issue
+    """
+    label_url = url + "/labels"
+    headers = {"Authorization": "Bearer " + os.environ['INPUT_TOKEN']}
+    response = requests.post(label_url, headers=headers, data=json.dumps({"labels": [label]}))
+    if response.status_code != 200:
+        raise Exception("Failed to add label to issue")
 
 def parse_commit_msg(url):
     """ parse url
@@ -25,9 +35,11 @@ def parse_commit_msg(url):
         print(msg)
         if find_signed_off_by(msg):
             print("Signed-off-by found")
+            add_label(os.environ['INPUT_ISSUE_URL'])
             return True
         else:
             print("Signed-off-by not found")
+            add_label(os.environ['INPUT_ISSUE_URL'], "No Signed-off-by")
             return False
 
 
